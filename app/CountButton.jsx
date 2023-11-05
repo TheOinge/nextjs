c0a13f4704e493c9eff9e99f5296726dfcfa9283
Logan <ltrud78@gmail.com>
2023-11-04 18:53:10 -0600

    this didnt work as intended

--- app/CountButton.jsx
+++ app/CountButton.jsx
@@ -2,18 +2,26 @@ import { useState, useEffect } from "react"
  
  export default function CountButton() {
     let [count, setCount] = useState(0)
+    
     function handleClick() {
-        setCount(count + 1)
-    }
+        if (count % 2 === 0) {
+          setCount(count + 1);
+        } else {
+          setCount(count + 2);
+        }
+      }
+    
     function rollOverCount(){
         if(count > 10) {
             setCount(0)
         }
     }
+   
     useEffect(rollOverCount, [count])
+   
     return (
       <div>
-      <button onClick={handleClick}>+1</button>
+      <button onClick={handleClick}>Increment</button>
       <div>{count}</div>
       </div>
     )
